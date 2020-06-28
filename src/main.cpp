eb82e58954d9e924dd8349b6c0fc1bdcb29e1a7b
Matthieu Aubry <aubry@cervval.com>
2020-06-28 22:04:27 +0200

    Digikeyboard lock screen (Win+L)

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
-#include <Arduino.h>
-
-const int LED = 1;
+#include "DigiKeyboardFr.h"
 
 void setup()
 {
-  pinMode(LED, OUTPUT);
+  DigiKeyboard.update();
+  DigiKeyboard.sendKeyStroke(0);
 }
 
 void loop()
 {
-  digitalWrite(LED, LOW);
-  delay(500);
-  digitalWrite(LED, HIGH);
-  delay(500);
+  // every charater that thislibrary can type
+  DigiKeyboard.sendKeyStroke(KEY_L, MOD_GUI_LEFT);
+
+  // It's better to use DigiKeyboard.delay() over the regular Arduino delay()
+  // if doing keyboard stuff because it keeps talking to the computer to make
+  // sure the computer knows the keyboard is alive and connected
+  DigiKeyboardFr.delay(10000);
 }
